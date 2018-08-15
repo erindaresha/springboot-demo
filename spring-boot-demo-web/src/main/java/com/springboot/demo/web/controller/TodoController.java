74add17f5054e22d37b172e6db6f79460de2388a
GDN\indra.e.prasetya <indra.e.prasetya@gdn-commerce.com>
2018-08-16 00:25:17 +0700

    fix urls

--- spring-boot-demo-web/src/main/java/com/springboot/demo/web/controller/TodoController.java
+++ spring-boot-demo-web/src/main/java/com/springboot/demo/web/controller/TodoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class TodoController {
   @Autowired
   private TodoService todoService;
 
-  @PostMapping("/api/member")
+  @PostMapping("/api/todo")
   public Response<Todo> createTodo(
       @RequestParam String username,
       @RequestParam String title,
@@ -29,14 +30,14 @@ public class TodoController {
     return ResponseHelper.ok(todoService.createTodo(username, title, description));
   }
 
-  @DeleteMapping("/api/member")
+  @DeleteMapping("/api/todo")
   public Response<Boolean> deleteTodo(
       @RequestParam String username,
       @RequestParam String title) {
     return ResponseHelper.ok(todoService.deleteTodo(username, title));
   }
 
-  @PostMapping("/api/member")
+  @GetMapping("/api/todo")
   public Response<List<Todo>> getTodos(
       @RequestParam String username) {
     return ResponseHelper.ok(todoService.getMemberTodos(username));
